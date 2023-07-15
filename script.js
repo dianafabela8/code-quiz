c6909eb4f13f449de6a5a1801c677f0af2624672
dianafabela8 <fabeladiana.df@gmail.com>
2023-07-15 16:40:43 -0700

    added questions in js file

--- script.js
+++ script.js
@@ -19,6 +19,49 @@ var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
+//Questions
+
+var quizQuestions = [{
+        question: "True or False. A string is a series of characters and is surrounded by quotes.",
+        ChoiceA: "True",
+        ChoiceB: "False",
+        correctAnswer: "a"},
+    {
+        question: "How many values does booleans have?",
+        ChoiceA: "one",
+        ChoiceB: "two",
+        ChoiceC: "three",
+        ChoiceD: "four",
+        correctAnswer: "b"},
+    
+    {
+        question: "Fill in the blank. Modulus returns the ___ between two numbers.",
+        ChoiceA: "Whole number",
+        ChoiceB: "Boolean",
+        ChoiceC: "Remainder",
+        ChoiceD: "String",
+        correctAnswer: "c"},
+    {
+        question: "What do we use to store groups of data in a single variable?",
+        ChoiceA: "Iteration",
+        ChoiceB: "Arrays",
+        ChoiceC: "Functions",
+        ChoiceD: "Objects",
+        correctAnswer: "b"},
+    {
+        question: "What happens when the same variable is used in the local and global scope?",
+        ChoiceA: "Shadowing",
+        ChoiceB: "Null",
+        ChoiceC: "Iteration",
+        ChoiceD: "Error",
+        correctAnswer: "a"},
+    {
+        question: "True or False. Objects are a collection of properties.",
+        ChoiceA: "True",
+        ChoiceB: "False",
+        correctAnswer: "a"},
+];
+
 
 
 
