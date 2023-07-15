e697e592e85e007134e3fc244ce7409d80afc3aa
dianafabela8 <fabeladiana.df@gmail.com>
2023-07-15 15:44:50 -0700

    added variables in js

---
+++ script.js
@@ -0,0 +1,26 @@
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var quizBody = document.getElementById("quiz");
+var quizTimer = document.getElementById("timer");
+var questionsEl = document.getElementById("questions");
+var highscoreDisplayName = document.getElementById("highscore-initals");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreInputName = document.getElementById("initials");
+var resultsEl = document.getElementById("result");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var finalScoreEl = document.getElementById("finalScore");
+var submiteScoreBtn = document.getElementById("submitScore");
+var gameoverDiv = document.getElementById("gameover");
+var endGameBtns = document.getElementById("endGameBtns");
+
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+
+
+
+
+
